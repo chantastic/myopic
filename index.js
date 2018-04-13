77d2bbc5c5f07e010f794834632a18a896fa43a7
Michael Chan <mijoch@gmail.com>
2018-04-13 13:33:38 -0700

    init: myopic

---
+++ index.js
@@ -0,0 +1,3 @@
+export default function myopic(thingIveJustLearned, thingIveUsed) {
+  return `${thingIveJustLearned} replaces ${thingIveUsed}. I don't see any reason to use ${thingIveUsed} anymore.`;
+}
